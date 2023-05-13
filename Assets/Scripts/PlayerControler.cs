0927dfbd3623a623458104b21c2aa1b53658a069
needitem <th07290828@gmail.com>
2023-05-13 11:04:11 +0900

    공격알고리즘 변경

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
 
+    [SerializeField] private float curTime;
+    public float coolTime = 0.2f;
+    public Vector2 boxSize;
+    public Transform pos;
+
     void Start()
     {
     }
 
     void Update()
     {
-        transform.GetChild(0).gameObject.SetActive(false);
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.GetChild(0).gameObject.SetActive(true);
+            if(curTime <= 0)
+            {
+                Collider2D[] colliders = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+                foreach (Collider2D collider in colliders)
+                {
+                    Debug.Log(collider.tag);
+                    Destroy(collider.gameObject);
+                }
+
+                //animator.SetTrigger("attack");
+                curTime = coolTime;
+            }
+            
+            
         }
-        
+        curTime -= Time.deltaTime;
+
     }
 
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(pos.position, boxSize);
+    }
 
 }
