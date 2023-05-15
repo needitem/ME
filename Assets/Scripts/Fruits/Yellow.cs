91cd61a22472e479de04002f9cb864b152e27820
kimjin0816 <124766994+kimjin0816@users.noreply.github.com>
2023-05-15 10:54:56 +0900

    베지어곡선 추가

--- Assets/Scripts/Fruits/Yellow.cs
+++ Assets/Scripts/Fruits/Yellow.cs
@@ -3,24 +3,17 @@ using UnityEngine;
 public class Yellow : MonoBehaviour
 {
     [SerializeField] private float pushPower;
-    [SerializeField] public int hp = 2;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void FixedUpdate()
     {
         // Move the object to the right
         Vector2 movement = new Vector2(3, 2);
         rb.AddForce(movement * pushPower, ForceMode2D.Impulse);
-
-        if (hp <= 0)
-        {
-            Destroy(gameObject);
-        }
     }
-
-
 }
