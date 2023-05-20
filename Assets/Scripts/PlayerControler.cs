04be309fc06daf95f0291f28a4b6874ecb2108a6
CarvingMan <130010242+CarvingMan@users.noreply.github.com>
2023-05-20 22:11:06 +0900

    kijun

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -24,25 +24,34 @@ public class PlayerControler : MonoBehaviour
     float fUpSize; //증가시킬 사이즈
     bool isUpScale = false;
     GameObject gBackFruit;
-
+    Animator PlayerAnimator; // 플레이어 애니메이터
 
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-        fUpSize = 0.2f;
+        fUpSize = 1.1f;
+        this.PlayerAnimator = GetComponent<Animator>(); // 애니메이터  컴포넌트를 가져온다.
+        
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Attack();
         }
-        else if (Input.GetButtonDown("Fire1"))
+        
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             PunchBackColliders();
-            //Upscale();
+            this.PlayerAnimator.SetTrigger("punch");
+           
+        }
+
+        if (isUpScale == true) {
+            Upscale();
         }
     }
 
@@ -54,7 +63,7 @@ public class PlayerControler : MonoBehaviour
             Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
             if (rigidbody != null)
             {
-                rigidbody.AddForce(new Vector2(-1, 1) * pushPower, ForceMode2D.Impulse);
+                rigidbody.AddForce(new Vector2(1, 1) * pushPower, ForceMode2D.Impulse);
                 this.gBackFruit = collider.gameObject;
                 isUpScale = true;
             }
@@ -68,13 +77,13 @@ public class PlayerControler : MonoBehaviour
         {
             //튕겨내면 2d지만 z축으로 튕겨내기에 원근법을 사용하여 시각적인 입체감을 준다.
             gBackFruit.transform.localScale = new Vector3(fUpSize, fUpSize, 0);
-            fUpSize += 0.1f; //사이즈 증가
+            fUpSize += 0.05f; //사이즈 증가
         }
 
-        if (fUpSize >= 3)
+        if (fUpSize >= 5)
         {
             Destroy(gBackFruit);
-            fUpSize = 0.2f;
+            fUpSize = 1.1f;
             isUpScale = false;
         }
     }
@@ -87,33 +96,34 @@ public class PlayerControler : MonoBehaviour
         float currentTime = Time.time;
         if (!isdoubleAttack) // 더블어택을 사용하지 않았다면 실행
         {
-            if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow) // 1초 안에 스페이스바를 두번 누른상태이고, 두번째 공격을 누른시간(currentTime)에서 
+            if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow) // 0.5초 안에 스페이스바를 두번 누른상태이고, 두번째 공격을 누른시간(currentTime)에서 
             {                                                                            // 첫번째 공격을 누른시간(lastAttackTime) 사이의 시간차이가 0.3초 보다 작다면 더블어택 실행
                                                                                          // (쉽게 말해 스페이스 따닥 누른시간의 간격이 0.3보다 작으면 실행)
-
+                this.PlayerAnimator.SetTrigger("double_attack");
                 isdoubleAttack = true; // 더블어택을 사용했다는 뜻                                 
 
                 // 더블 어택 실행      
                 Debug.Log("doubleAttack");
                 //anim.SetTrigger("doubleAttack");           
-                Invoke("Delay", 1f); // 1초후 실행
+                Invoke("Delay", 0.5f); // 1초후 실행
 
 
             }
             else if (!hasAttacked)
             {
+                this.PlayerAnimator.SetTrigger("attack");
                 // 어택 실행
                 Debug.Log("Attack");
                 //anim.SetTrigger("attack");
                 hasAttacked = true;
                 lastAttackTime = currentTime; // 첫번째 공격시간을 lastAttackTime에 저장
-                Invoke("Delay", 1f); // 1초후 실행
+                Invoke("Delay", 0.5f); // 0.5초후 실행
                 Debug.Log("===============================");
             }
         }
         else // 더블어택을 사용했다면 실행
         {
-            Invoke("TransIsdoubleAttack", 1f); // 1초후 실행
+            Invoke("TransIsdoubleAttack", 0.5f); // 0.5초후 실행
         }
     }
 
