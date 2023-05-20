b2d13bc8d23d02990b953f42ebe4bae187678b63
lsp6359 <lsp6359@naver.com>
2023-05-20 16:49:01 +0900

    Attack함수 변경

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -16,6 +16,10 @@ public class PlayerControler : MonoBehaviour
     public Animator animator;
     private Rigidbody2D rb;
 
+    //승표
+    bool isdoubleAttack = false; // 더블어택중인지 아닌지 보는 변수
+
+
     //기준
     float fUpSize; //증가시킬 사이즈
     bool isUpScale = false;
@@ -73,27 +77,48 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
-    public void Attack() // 일반 공격인지, 2회 연속 공격 판단함수
-
+    public void Attack() // 일반 공격인지, 2회 연속 공격인지 구분
     {
         float currentTime = Time.time;
-        if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow) // 1초 안에 스페이스바를 두번 누른상태이고, 두번째 공격을 누른시간(currentTime)에서 
-                                                                                     // 첫번째 공격을 누른시간(lastAttackTime) 사이의 시간차이가 0.3초 보다 작다면 더블어택 실행
+        if (!isdoubleAttack) // 더블어택을 사용하지 않았다면 실행
         {
-            // 더블 어택 실행
-            Debug.Log("doubleAttack");
-            //anim.SetTrigger("doubleAttack");
-            hasAttacked = false;
-        }
-        else if(!hasAttacked)
-        {
-            Debug.Log("Attack");
-            //anim.SetTrigger("attack");
-            hasAttacked = true;
-            lastAttackTime = currentTime; // 첫번째 공격시간을 lastAttackTime에 저장
-            StartCoroutine(ResetAttack()); // 코루틴 함수 호출, 1초 후 hasAttacked를 false로 바꾼다는 의미
+            if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow) // 1초 안에 스페이스바를 두번 누른상태이고, 두번째 공격을 누른시간(currentTime)에서 
+            {                                                                            // 첫번째 공격을 누른시간(lastAttackTime) 사이의 시간차이가 0.3초 보다 작다면 더블어택 실행
+                                                                                         // (쉽게 말해 스페이스 따닥 누른시간의 간격이 0.3보다 작으면 실행)
 
+                isdoubleAttack = true; // 더블어택을 사용했다는 뜻                                 
+                
+                // 더블 어택 실행      
+                Debug.Log("doubleAttack");            
+                //anim.SetTrigger("doubleAttack");           
+                Invoke("Delay", 1f); // 1초후 실행
+
+
+            }
+            else if (!hasAttacked)
+            {
+                // 어택 실행
+                Debug.Log("Attack");            
+                //anim.SetTrigger("attack");
+                hasAttacked = true;
+                lastAttackTime = currentTime; // 첫번째 공격시간을 lastAttackTime에 저장
+                Invoke("Delay", 1f); // 1초후 실행                     
+            }
         }
+        else // 더블어택을 사용했다면 실행
+        {
+            Invoke("TransIsdoubleAttack", 1f); // 1초후 실행
+        }
+    }
+
+    void Delay() // hasAttacked를 false로 변경
+    {
+        hasAttacked = false;
+    }
+
+    void TransIsdoubleAttack() // isdoubleAttack을 false로 변경
+    {
+        isdoubleAttack = false;
     }
 
     IEnumerator ResetAttack() // 코루틴 함수
