88ef9eb702d77542f8f9878fa1e421312244f097
CarvingMan <130010242+CarvingMan@users.noreply.github.com>
2023-05-20 17:31:00 +0900

    기주니

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -16,10 +16,6 @@ public class PlayerControler : MonoBehaviour
     public Animator animator;
     private Rigidbody2D rb;
 
-    //승표
-    bool isdoubleAttack = false; // 더블어택중인지 아닌지 보는 변수
-
-
     //기준
     float fUpSize; //증가시킬 사이즈
     bool isUpScale = false;
@@ -27,11 +23,11 @@ public class PlayerControler : MonoBehaviour
 
 
 
-    private void Start() {
+    private void Start()
+    {
         animator = GetComponent<Animator>();
         fUpSize = 0.2f;
     }
-
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
@@ -41,95 +37,78 @@ public class PlayerControler : MonoBehaviour
         else if (Input.GetButtonDown("Fire1"))
         {
             PunchBackColliders();
-            //Upscale();
         }
+
+
+        Upscale();
     }
+
+
 
     private void PunchBackColliders()
     {
         var colliders = Physics2D.OverlapBoxAll(pos.position, boxSize, 0).ToList();
         foreach (Collider2D collider in colliders)
         {
+
             Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
             if (rigidbody != null)
             {
-                rigidbody.AddForce(new Vector2(-1,1) * pushPower, ForceMode2D.Impulse);
+                rigidbody.AddForce(new Vector2(1, 1) * pushPower, ForceMode2D.Impulse);
                 this.gBackFruit = collider.gameObject;
                 isUpScale = true;
             }
         }
     }
 
-    void Upscale() {
+    void Upscale()
+    {
 
         if (isUpScale == true)
         {
             //튕겨내면 2d지만 z축으로 튕겨내기에 원근법을 사용하여 시각적인 입체감을 준다.
             gBackFruit.transform.localScale = new Vector3(fUpSize, fUpSize, 0);
+
             fUpSize += 0.1f; //사이즈 증가
         }
 
-        if (fUpSize >= 3)
+
+        if (fUpSize >= 6)
         {
             Destroy(gBackFruit);
             fUpSize = 0.2f;
             isUpScale = false;
         }
+
     }
 
-    public void Attack() // 일반 공격인지, 2회 연속 공격인지 구분
+    public void Attack()
     {
         float currentTime = Time.time;
-        if (!isdoubleAttack) // 더블어택을 사용하지 않았다면 실행
+        if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow)
         {
-            if (hasAttacked && (currentTime - lastAttackTime) <= doubleAttackTimeWindow) // 1초 안에 스페이스바를 두번 누른상태이고, 두번째 공격을 누른시간(currentTime)에서 
-            {                                                                            // 첫번째 공격을 누른시간(lastAttackTime) 사이의 시간차이가 0.3초 보다 작다면 더블어택 실행
-                                                                                         // (쉽게 말해 스페이스 따닥 누른시간의 간격이 0.3보다 작으면 실행)
-
-                isdoubleAttack = true; // 더블어택을 사용했다는 뜻                                 
-                
-                // 더블 어택 실행      
-                Debug.Log("doubleAttack");            
-                //anim.SetTrigger("doubleAttack");           
-                Invoke("Delay", 1f); // 1초후 실행
-
-
-            }
-            else if (!hasAttacked)
-            {
-                // 어택 실행
-                Debug.Log("Attack");            
-                //anim.SetTrigger("attack");
-                hasAttacked = true;
-                lastAttackTime = currentTime; // 첫번째 공격시간을 lastAttackTime에 저장
-                Invoke("Delay", 1f); // 1초후 실행                     
-            }
+            Debug.Log("doubleAttack");
+            //anim.SetTrigger("doubleAttack");
+            hasAttacked = false;
         }
-        else // 더블어택을 사용했다면 실행
+        else if (!hasAttacked)
         {
-            Invoke("TransIsdoubleAttack", 1f); // 1초후 실행
+            Debug.Log("Attack");
+            //anim.SetTrigger("attack");
+            hasAttacked = true;
+            lastAttackTime = currentTime;
+            StartCoroutine(ResetAttack());
         }
     }
-
-    void Delay() // hasAttacked를 false로 변경
+    IEnumerator ResetAttack()
     {
+        yield return new WaitForSeconds(coolTime);
         hasAttacked = false;
     }
 
-    void TransIsdoubleAttack() // isdoubleAttack을 false로 변경
-    {
-        isdoubleAttack = false;
-    }
-
-    //IEnumerator ResetAttack() // 코루틴 함수
-    //{
-    //    yield return new WaitForSeconds(coolTime); // 1초 후 hasAttacked 를 false로 바꾸겠다.
-
-    //    hasAttacked = false;
-    //}
-
     //onHit
-    private void OnTriggerEnter2D(Collider2D collider) {
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
         if (collider.tag == "Target")
         {
             Destroy(collider.gameObject);
@@ -141,4 +120,5 @@ public class PlayerControler : MonoBehaviour
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube(pos.position, boxSize);
     }
+
 }
