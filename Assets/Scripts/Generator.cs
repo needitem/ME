4060fbb385964f15e98bf29e391d43d620477775
CYHcode05 <133067546+CYHcode05@users.noreply.github.com>
2023-05-29 13:52:10 +0900

    '.'

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Generator : MonoBehaviour
 {
+    System.Random rRand = new System.Random();
     // Start is called before the first frame update
     public GameObject[] fruits;
-    private float timeElapsed = 0f;
-    public float span = 4f;
+    private double dtimeElapsed = 0.0d;
+    public double dbpm = 0.0d; // bpm 
 
     //private int difficulty = 1;
     void Start()
@@ -18,18 +21,8 @@ public class Generator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeElapsed += Time.deltaTime;
-        if (timeElapsed >= span)
-        {
-            SpawnFruit();
-            timeElapsed = 0f;
-        }
+        ObjectThrow();
 
-        /*       if (timeElapsed >= 60d/ span)
-                {
-                    SpawnFruit();
-                    timeElapsed -= 60d / span;
-                }*/
     }
 
     private void SpawnFruit()
@@ -37,5 +30,17 @@ public class Generator : MonoBehaviour
         int randomIndex = Random.Range(0, fruits.Length);
         Vector3 spawnPosition = new Vector3(15, 1.5f, 1);
         Instantiate(fruits[randomIndex], spawnPosition, Quaternion.identity);
+    }
+
+    public void ObjectThrow()
+    {
+        dtimeElapsed += Time.deltaTime;
+        dbpm = Random.Range(120, 150); 
+        if (dtimeElapsed >= 60d / dbpm) // 1초당 나오는 노트수 ex) 120 bpm이면 1초당 2개
+        {
+            SpawnFruit();
+            dtimeElapsed -= 60d / dbpm;
+
+        }
     }
 }
