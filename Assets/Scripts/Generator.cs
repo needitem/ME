ef5cdf1e3621089101d2287a7551372fd72f1696
CYHcode05 <133067546+CYHcode05@users.noreply.github.com>
2023-06-17 14:13:32 +0900

    '.'

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,106 +1,49 @@
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-//(1.0 ~ 0.8) 사이로 만들고 함수 정리 
+
 public class Generator : MonoBehaviour
 {
-<<<<<<< HEAD
-    public GameObject[] gFruits  = null;
-    public GameObject[] gMainfood;
-    public GameObject[] gSubfood;
-
-    [SerializeField]
-    public GameObject gSuccessCount;
-
-    [SerializeField]
-    private GameObject gSpawn;
-    
-    [SerializeField]
-    private GameObject gNPC;
-    
-    [SerializeField]
-    private float[][] fSpanArray = new float[][]
-    {
-        new float[] {1.5f, 1.5f, 1.5f , 1.5f},
-        new float[] {1.3f, 1.2f, 1.3f, 1.2f},
-        new float[] {1.5f, 1.1f, 1.4f, 1.5f},
-        new float[] {1.3f, 1.2f, 1.0f, 1.2f, 1.3f},
-        new float[] {1.4f , 1.1f, 1.1f, 1.4f, 1.0f},
-        new float[] {1.2f, 1.1f, 1.5f, 1.2f, 1.1f}
-    };
-
-   
-    // private float[] fSpeed = new float[] { 1.0f, 0.9f,0.8f,0.7f,0.6f,0.55f,0.5f}; if recipe success ,spanArray speed fast  
-    [SerializeField]
-    private float fTimeElapsed = 0.0f;
-    [SerializeField]
-    private int iRowIndex = 0;
-    [SerializeField]
-    private int iColIndex = 0;
-
-    void Start()
-    {
-        gMainfood = Resources.LoadAll<GameObject>("Prefabs/MainFood");
-        gSubfood = Resources.LoadAll<GameObject>("Prefabs/SubFood");
-        iRowIndex = UnityEngine.Random.Range(0, fSpanArray.Length);
-        gNPC = GameObject.Find("NPC");
-=======
     public GameObject[] mainFood;
     public GameObject[] subFood;
     private GameObject spawn;
     private GameObject NPC;
     private float[][] spanArray = new float[][]
     {
-        new float[] {1.0f, 1.0f, 1.0f, 1.0f},
-        new float[] {1.0f, 0.5f, 0.5f, 1.0f, 1.0f},
-        new float[] {0.4f, 0.4f, 0.7f, 0.5f, 1.0f},
-        new float[] {0.25f, 0.25f, 0.6f, 0.8f, 0.6f, 1.0f, 0.5f},
-        new float[] {0.6f, 0.9f, 0.5f, 1.0f}
+        new float[] {1.5f, 1.5f, 1.5f, 1.5f},
+        new float[] {1.5f, 1.3f, 1.2f, 1.5f,},
+        new float[] {1.5f, 1.2f, 1.2f, 1.3f, 1.3f},
+        new float[] {1.3f, 1.2f, 1.3f, 1.2f , 1.5f},
+        new float[] {1.1f , 1.4f, 1.2f, 1.3f, 1.5f},
+        new float[] {1.2f, 1.3f, 1.4f, 1.5f, 1.1f}
     };
 
     private float timeElapsed = 0f;
     private int rowIndex = 0;
     private int colIndex = 0;
 
-    public float span = 1.3f;
-
     void Start()
     {
-        mainFood = Resources.LoadAll<GameObject>("Prefabs/MainFood"); 
-        subFood = Resources.LoadAll<GameObject>("Prefabs/SubFood"); 
+        mainFood = Resources.LoadAll<GameObject>("Prefabs/MainFood");
+        subFood = Resources.LoadAll<GameObject>("Prefabs/SubFood");
         rowIndex = Random.Range(0, spanArray.Length);
         NPC = GameObject.Find("NPC");
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
     }
 
     void Update()
     {
-<<<<<<< HEAD
-        fTimeElapsed += Time.deltaTime;
-        if (fTimeElapsed >= GetCurrentSpan() && GameDirector.hp > 0)
-        {
-            gNPC.GetComponent<NPCController>().Drawing();
-            SpawnFood();
-            fTimeElapsed = 0;
-=======
         timeElapsed += Time.deltaTime;
         if (timeElapsed >= GetCurrentSpan() && GameDirector.hp > 0) // generate food every span seconds
         {
             NPC.GetComponent<NPCController>().Drawing(); // NPC throw food animation
             SpawnFood();
             timeElapsed = 0; // reset timer
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
 
-            if (iColIndex == fSpanArray[iRowIndex].Length - 1)
+            if (colIndex == spanArray[rowIndex].Length - 1)
             {
                 UpdateIndices(); // update indices for next spawn
             }
             else
             {
-                iColIndex++;
+                colIndex++;
             }
         }
     }
@@ -111,22 +54,6 @@ public class Generator : MonoBehaviour
         GameObject foodPrefab;
         int itemHp;
 
-<<<<<<< HEAD
-        if (UnityEngine.Random.Range(0, 3) == 0)
-        {
-            foodPrefab = gMainfood[UnityEngine.Random.Range(0, gMainfood.Length)];
-            itemHp = 2;
-        }
-        else
-        {
-            foodPrefab = gSubfood[UnityEngine.Random.Range(0, gSubfood.Length)];
-            itemHp = 1;
-        }
-
-        gSpawn = Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
-        gSpawn.name = foodPrefab.name;
-        gSpawn.GetComponent<ItemController>().itemHp = itemHp;
-=======
         if (Random.Range(0, 3) == 0) // 1/3 chance to spawn main food. set bigger value to increase chance
         {
             foodPrefab = mainFood[Random.Range(0, mainFood.Length)];
@@ -142,26 +69,16 @@ public class Generator : MonoBehaviour
         spawn = Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
         spawn.name = foodPrefab.name; // set name of food to its prefab name. prevent (prefab) in hierarchy
         spawn.GetComponent<ItemController>().itemHp = itemHp;
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
     }
 
     private float GetCurrentSpan() // get current span
     {
-        gSuccessCount = GameObject.Find("gameDirector");
-        return fSpanArray[iRowIndex][iColIndex];//* fSpeed[gSuccessCount.GetComponent<Recipe>().SuccessRecipe];
+        return spanArray[rowIndex][colIndex];
     }
 
     private void UpdateIndices()
     {
-<<<<<<< HEAD
-        iRowIndex = UnityEngine.Random.Range(0, fSpanArray.Length);
-        iColIndex = 0;
-    }
-
-
-=======
         rowIndex = Random.Range(0, spanArray.Length); // update row index
         colIndex = 0;
     }
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
 }
--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -11,46 +11,21 @@ public enum Ingredients
     green_onion,
     egg,
     onion,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    green_onion,
->>>>>>> 1462d7f0dc647633f5280c97ac77c37cb3af07b7
-=======
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
     carrot
 }
 
 public class Recipe : MonoBehaviour
 {
-<<<<<<< HEAD
-    // Define recipes
-    GameObject gameDirector;
-    const int ingredientAmt = 7;
-    private int[] r1;
-    private int[] r2;
-    private int[] r3;
-    public static int[] randomRecipe = new int[ingredientAmt];
-<<<<<<< HEAD
-    public int SuccessRecipe = 0;
-=======
-=======
     [SerializeField] private GameObject gameDirector;
     private const int ingredientAmt = 7;
     private readonly int[] recipe1 = { 20, 0, 0, 0, 0, 10, 10 };
     private readonly int[] recipe2 = { 0, 10, 0, 10, 10, 10, 0 };
     private readonly int[] recipe3 = { 0, 0, 10, 10, 0, 10, 0 };
     private static readonly int[] randomRecipe = new int[ingredientAmt];
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
 
     public static int RecipeIndex { get; private set; }
 
-<<<<<<< HEAD
->>>>>>> 1462d7f0dc647633f5280c97ac77c37cb3af07b7
-    public bool IsRecipeComplete(int[] randomRecipe)
-=======
     public bool IsRecipeComplete(int[] randomRecipe) // check if recipe is complete, return true if complete, return false if not
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
     {
         foreach (int i in randomRecipe)
         {
@@ -76,22 +51,9 @@ public class Recipe : MonoBehaviour
 
     private void Awake() // initialize randomRecipe
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        r1 = new int[ingredientAmt] { 1, 0, 0, 0, 0, 1, 1 };  // ·¹½ÃÇÇ 1: beef + onion + carrot
-        r2 = new int[ingredientAmt] { 0, 1, 0, 1, 1, 1, 0 };  // ·¹½ÃÇÇ 2: chicken + onion + depa + egg
-        r3 = new int[ingredientAmt] { 0, 0, 1, 1, 0, 1, 0 };  // ·¹½ÃÇÇ 3: fish + depa + onion
-=======
-        r1 = new int[ingredientAmt] { 2, 0, 0, 0, 0, 1, 1 };  // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 1: beef + onion + carrot
-        r2 = new int[ingredientAmt] { 0, 1, 0, 1, 1, 1, 0 };  // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 2: chicken + onion + depa + egg
-        r3 = new int[ingredientAmt] { 0, 0, 1, 1, 0, 1, 0 };  // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 3: fish + depa + onion
->>>>>>> 1462d7f0dc647633f5280c97ac77c37cb3af07b7
-        randomRecipe = new int[ingredientAmt];
-=======
         Init();
         gameDirector = GameObject.Find("GameDirector");
         RecipeIndex = CreateRandomRecipe();
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
     }
 
     private void Update()
@@ -108,7 +70,7 @@ public class Recipe : MonoBehaviour
             }
             else if (IsRecipeWrong(randomRecipe))
             {
-               //GameDirector.hp = 0;
+                //GameDirector.hp = 0;
             }
         }
     }
@@ -120,7 +82,7 @@ public class Recipe : MonoBehaviour
 
     private int CreateRandomRecipe() // create random recipe and return index of recipe
     {
-        int randomIndex = Random.Range(0, 7);
+        int randomIndex = Random.Range(0, 3);
 
         switch (randomIndex)
         {
@@ -140,68 +102,12 @@ public class Recipe : MonoBehaviour
 
     public static Dictionary<int, int> ShowLeftoverRecipe() // return leftover recipe
     {
-<<<<<<< HEAD
-        //display
-    }
-
-    public static void decreaseIngredient(string name)
-    {
-        Ingredients ingredient = (Ingredients)Enum.Parse(typeof(Ingredients), name);
-        int index = (int)ingredient;
-        randomRecipe[index]--;
-        /*        Debug.Log(index);
-                Debug.Log(randomRecipe[index]);
-                Debug.Log(ingredient);*/
-    }
-
-    private void Start()
-    {
-        gameDirector = GameObject.Find("GameDirector");
-        init();
-<<<<<<< HEAD
-        createRandomRecipe();
-=======
-        RecipeIndex = createRandomRecipe();
->>>>>>> 1462d7f0dc647633f5280c97ac77c37cb3af07b7
-    }
-
-    private void Update()
-    {        // Recipe reset status
-        if (IsRecipeComplete(randomRecipe) || IsRecipeWrong(randomRecipe))
-        {
-            init();
-<<<<<<< HEAD
-            createRandomRecipe();
-            /*            Debug.Log("·¹½ÃÇÇ ´Ù½Ã »ý¼º");*/
-            gameDirector.GetComponent<GameDirector>().UpdateUI(gameDirector.GetComponent<GameDirector>().GetRecipeIndex());
-            if (IsRecipeComplete(randomRecipe))
-            {
-                while(SuccessRecipe != 9)
-                {
-                    SuccessRecipe++;
-                    break;
-                }
-                
-=======
-            RecipeIndex = createRandomRecipe();
-            /*            Debug.Log("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ù½ï¿½ ï¿½ï¿½ï¿½ï¿½");*/
-            gameDirector.GetComponent<GameDirector>().UpdateUI(gameDirector.GetComponent<GameDirector>().GetIngredientIndex());
-            if (IsRecipeComplete(randomRecipe))
-            {
->>>>>>> 1462d7f0dc647633f5280c97ac77c37cb3af07b7
-                // Success Performance;
-            }
-            else
-            {
-                // Fail Performance;
-=======
         Dictionary<int, int> temp = new Dictionary<int, int>();
         for (int i = 0; i < randomRecipe.Length; i++) // copy randomRecipe to temp
         {
             if (randomRecipe[i] > 0) // if randomRecipe[i] is not 0, add to temp
             {
                 temp.Add(i, randomRecipe[i]);
->>>>>>> 037113a09a7e88abd2eaff2a99f17905796a941d
             }
         }
         return temp;
