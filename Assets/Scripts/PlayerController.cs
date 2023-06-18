948880ad435e43ee1fa92185d421c03e0cf55968
kimtaeho <th07290828@gmail.com>
2023-06-18 21:48:24 +0900

    a

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,11 +17,8 @@ public class PlayerController : MonoBehaviour
     Animator playerAnimator;
     AudioDirector audioDirector;
 
-    GameObject Gameover;
-
     private void Start()
     {
-        Gameover = GameObject.Find("Gameover");
         playerAnimator = GetComponent<Animator>();
         audioDirector = GetComponent<AudioDirector>();
     }
@@ -39,51 +36,52 @@ public class PlayerController : MonoBehaviour
 
         if (GameDirector.hp <= 0)
         {
-/*            Gameover.GetComponent<Gameover_sound>().gameover();
-*/            playerAnimator.SetTrigger("game_over");
-/*            audioDirector.SoundMute(true);*/
+            playerAnimator.SetTrigger("game_over");
         }
+
     }
     public void PunchBack() //effect of punching back ingredients
     {
-        isPunched = true;
-        playerAnimator.SetTrigger("punch");
-
-        var colliders = Physics2D.OverlapBoxAll(pos.position, boxSize, 0).ToList(); //get colliders in the box, and put them in the list
-        if (colliders.Count == 0) //if there is no collider in the box, play the sound of punching air
+        if (isDelay == false)
         {
             isPunched = true;
             playerAnimator.SetTrigger("punch");
-            
+
+
             var colliders = Physics2D.OverlapBoxAll(pos.position, boxSize, 0).ToList();
+
             foreach (Collider2D collider in colliders)
             {
+
                 if (collider.tag == "Target")
                 {
                     audioDirector.SoundPlay("Sound/effect_sound/fryingpan");
                     KatanaEffect.Punch();
                     Effect.Apply(collider.gameObject);
                 }
-                else {
+                else
+                {
                     audioDirector.SoundPlay("Sound/effect_sound/swing1");
                 }
             }
             isDelay = true;
             StartCoroutine(CountAttackDelay(0.4f));
         }
+
     }
 
-    public void Attack() //
+    public void Attack()
     {
         hasAttacked = true;
         float currentTime = Time.time;
 
         var colliders = Physics2D.OverlapBoxAll(pos.position, boxSize, 0).ToList();
+
         if (!isDelay) //if attack delay is false, attack. attack delay is true when player attacks
         {
 
             playerAnimator.SetTrigger("attack");
-            
+
 
             foreach (Collider2D collider in colliders)
             {
@@ -93,19 +91,22 @@ public class PlayerController : MonoBehaviour
                     collider.gameObject.GetComponent<ItemController>().itemHp--;
                     audioDirector.SoundPlay("Sound/effect_sound/slice1");
                 }
-                else {
+                else
+                {
                     audioDirector.SoundPlay("Sound/effect_sound/swing1");
                 }
             }
 
             isDelay = true;
             lastAttackTime = currentTime; //reset the last attack time
-            StartCoroutine(CountAttackDelay(0.3f));
+            StartCoroutine(CountAttackDelay(0.4f));
         }
         else if ((currentTime - lastAttackTime) <= doubleAttackTimeWindow) //if player attacks again within 0.2 seconds
         {
             isDouble = true;
             playerAnimator.SetTrigger("double_attack");
+
+
 
             foreach (Collider2D collider in colliders)
             {
@@ -119,22 +120,34 @@ public class PlayerController : MonoBehaviour
                     KatanaEffect.DoubleAttack();
                     collider.gameObject.GetComponent<ItemController>().itemHp--;
                 }
-                else {
+                else
+                {
                     audioDirector.SoundPlay("Sound/effect_sound/swing2");
                 }
             }
             isDelay = true;
         }
-        StartCoroutine(CountAttackDelay(0.4f));
     }
 
-
-    IEnumerator CountAttackDelay(float delayTime)
+    void ResetDelay()
     {
-        yield return new WaitForSeconds(delayTime); //wait for delayTime seconds
         isDelay = false;
         isPunched = false;
         hasAttacked = false;
+    }
+
+    IEnumerator CountAttackDelay(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        if (isDouble == true) //                    ?        0.2 ?   ? .
+        {
+            Invoke("ResetDelay", 0.2f);
+            isDouble = false;
+        }
+        else if (isDouble == false)
+        {
+            ResetDelay();
+        }
 
     }
     void OnTriggerEnter2D(Collider2D collider)
@@ -146,11 +159,5 @@ public class PlayerController : MonoBehaviour
             audioDirector.SoundPlay("Sound/effect_sound/hit");
             playerAnimator.SetTrigger("damaged");
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(pos.position, boxSize);
     }
 }
