68db603a28c5bd8ca488f8a2dff72886e1763ca3
kimjin0816 <124766994+kimjin0816@users.noreply.github.com>
2023-06-19 09:10:09 +0900

    fix

--- Assets/Scripts/Fruits/ItemController.cs
+++ Assets/Scripts/Fruits/ItemController.cs
@@ -32,7 +32,11 @@ public class ItemController : MonoBehaviour
             Destroy(gameObject);
         }
 
+<<<<<<< Updated upstream
         // 선형보간을 계산한 결과값을 프레임과 프레임 사이의 시간을 계속 더해 이동
+=======
+        // ?�형보간??계산??결과값을 ?�레?�과 ?�레???�이???�간??계속 ?�해 ?�동
+>>>>>>> Stashed changes
         rate += Time.deltaTime;
         transform.position = BezierTest(controllPosition[0], controllPosition[1], controllPosition[2], controllPosition[3], rate);
         
@@ -66,6 +70,7 @@ public class ItemController : MonoBehaviour
 
     // https://www.youtube.com/watch?v=KTEX2L4T4zE
 
+<<<<<<< Updated upstream
     // 점과 점 사잇값을 추정하기 위하여 직선 거리에 따라 선형적으로 계산하는 방법이다.
     public Vector2 BezierTest(Vector2 P_1, Vector2 P_2, Vector2 P_3, Vector2 P_4, float value)
     {
@@ -78,6 +83,20 @@ public class ItemController : MonoBehaviour
 
         Vector2 F = Vector2.Lerp(D, E, value);      // D지점과 E지점의 선형구간 계산값 : F
         return F;                                   // 위에서 계산한 총 결과값을 F로 반환
+=======
+    // ?�과 ???�잇값을 추정?�기 ?�하??직선 거리???�라 ?�형?�으�?계산?�는 방법?�다.
+    public Vector2 BezierTest(Vector2 P_1, Vector2 P_2, Vector2 P_3, Vector2 P_4, float value)
+    {
+        Vector2 A = Vector2.Lerp(P_1, P_2, value);  // P_1지?�과 P_2지?�의 ?�형구간 계산�?: A
+        Vector2 B = Vector2.Lerp(P_2, P_3, value);  // P_2지?�과 P_3지?�의 ?�형구간 계산�?: B
+        Vector2 C = Vector2.Lerp(P_3, P_4, value);  // P_3지?�과 P_4지?�의 ?�형구간 계산�?: C
+
+        Vector2 D = Vector2.Lerp(A, B, value);      // A지?�과 B지?�의 ?�형구간 계산�?: D
+        Vector2 E = Vector2.Lerp(B, C, value);      // B지?�과 C지?�의 ?�형구간 계산�?: E
+
+        Vector2 F = Vector2.Lerp(D, E, value);      // D지?�과 E지?�의 ?�형구간 계산�?: F
+        return F;                                   // ?�에??계산??�?결과값을 F�?반환
+>>>>>>> Stashed changes
     }
 
 
@@ -90,16 +109,25 @@ public class Test_Editor : Editor
 {
     private void OnSceneGUI()
     {
+<<<<<<< Updated upstream
         // 현재 오브젝트 참조
         ItemController Generator = (ItemController)target;
 
         // 오브젝트 제어
         // 제어 포인트 0부터 3까지의 위치를 핸들을 통해 조정합니다.
+=======
+        // ?�재 ?�브?�트 참조
+        ItemController Generator = (ItemController)target;
+
+        // ?�브?�트 ?�어
+        // ?�어 ?�인??0부??3까�????�치�??�들???�해 조정?�니??
+>>>>>>> Stashed changes
         Generator.controllPosition[0] = Handles.PositionHandle(Generator.controllPosition[0], Quaternion.identity);
         Generator.controllPosition[1] = Handles.PositionHandle(Generator.controllPosition[1], Quaternion.identity);
         Generator.controllPosition[2] = Handles.PositionHandle(Generator.controllPosition[2], Quaternion.identity);
         Generator.controllPosition[3] = Handles.PositionHandle(Generator.controllPosition[3], Quaternion.identity);
 
+<<<<<<< Updated upstream
         // pos[0], pos[1] 연결
         // 제어 포인트 0과 1을 선으로 연결합니다.
         Handles.DrawLine(Generator.controllPosition[0], Generator.controllPosition[1]);
@@ -120,6 +148,28 @@ public class Test_Editor : Editor
             Vector2 After = Generator.BezierTest(Generator.controllPosition[0], Generator.controllPosition[1], Generator.controllPosition[2], Generator.controllPosition[3], value_After); ;
 
             // 이전 위치와 다음 위치를 선으로 연결합니다.
+=======
+        // pos[0], pos[1] ?�결
+        // ?�어 ?�인??0�?1???�으�??�결?�니??
+        Handles.DrawLine(Generator.controllPosition[0], Generator.controllPosition[1]);
+        // pos[2], pos[3] ?�결
+        // ?�어 ?�인??2?� 3???�으�??�결?�니??
+        Handles.DrawLine(Generator.controllPosition[2], Generator.controllPosition[3]);
+
+        // 카운?��? 증�??�에 ?�라 ??부?�럽�??�니??
+        int Count = 50;
+        for (float i = 0; i < Count; i++)
+        {
+            // Bezier 곡선???�라 ?�전 ?�치�?계산?�니??
+            float value_Before = i / Count;
+            Vector2 Before = Generator.BezierTest(Generator.controllPosition[0], Generator.controllPosition[1], Generator.controllPosition[2], Generator.controllPosition[3], value_Before);
+
+            // Bezier 곡선???�라 ?�음 ?�치�?계산?�니??
+            float value_After = (i + 1) / Count;
+            Vector2 After = Generator.BezierTest(Generator.controllPosition[0], Generator.controllPosition[1], Generator.controllPosition[2], Generator.controllPosition[3], value_After); ;
+
+            // ?�전 ?�치?� ?�음 ?�치�??�으�??�결?�니??
+>>>>>>> Stashed changes
             Handles.DrawLine(Before, After);
         }
     }
