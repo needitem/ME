9a5015a2448f825702db03a88a8dd4426bbf0983
NaHoSeok <133612683+NaHoSeok@users.noreply.github.com>
2023-07-24 19:05:10 +0900

    .
    .

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -6,22 +6,6 @@ using UnityEngine.SceneManagement;
 using System.Linq;
 public class GameDirector : MonoBehaviour
 {
-<<<<<<< HEAD
-    [SerializeField] public int maxHp = 3;              // ÇÃ·¹ÀÌ¾îÀÇ ÃÖ´ë HP
-    [SerializeField] public Image[] heartImages;        // ÇÃ·¹ÀÌ¾îÀÇ HP¸¦ ³ªÅ¸³»´Â ÇÏÆ® ÀÌ¹ÌÁö ¹è¿­
-    [SerializeField] GameObject[] gIngredient_cnt;      // Àç·á °³¼ö ÅØ½ºÆ®¸¦ ³ªÅ¸³»´Â °ÔÀÓ ¿ÀºêÁ§Æ® ¹è¿­
-    [SerializeField] public GameObject Gameover_Panel;  // °ÔÀÓ ¿À¹ö ÆÐ³Î ¿ÀºêÁ§Æ®
-
-    public Recipe recipe;                   // ·¹½ÃÇÇ ½ºÅ©¸³Æ® ÂüÁ¶
-    public Image recipeImage;               // ·¹½ÃÇÇ ÀÌ¹ÌÁö ÄÄÆ÷³ÍÆ®
-    public Image[] ingredientImages;        // Àç·á ÀÌ¹ÌÁö ÄÄÆ÷³ÍÆ® ¹è¿­
-
-    public Sprite[] recipeSprites;          // ·¹½ÃÇÇ ½ºÇÁ¶óÀÌÆ® ¹è¿­
-    public Sprite[] ingredientSprites;      // Àç·á ½ºÇÁ¶óÀÌÆ® ¹è¿­
-
-
-    static public int hp;   // ÇÃ·¹ÀÌ¾îÀÇ ÇöÀç hp(static)
-=======
     [SerializeField] public int maxHp = 3;              // í”Œë ˆì´ì–´ì˜ ìµœëŒ€ HP
     [SerializeField] public Image[] heartImages;        // í”Œë ˆì´ì–´ì˜ HPë¥¼ ë‚˜íƒ€ë‚´ëŠ” í•˜íŠ¸ ì´ë¯¸ì§€ ë°°ì—´
     [SerializeField] GameObject[] gIngredient_cnt;      // ìž¬ë£Œ ê°œìˆ˜ í…ìŠ¤íŠ¸ë¥¼ ë‚˜íƒ€ë‚´ëŠ” ê²Œìž„ ì˜¤ë¸Œì íŠ¸ ë°°ì—´
@@ -36,7 +20,6 @@ public class GameDirector : MonoBehaviour
 
 
     static public int hp;   // í”Œë ˆì´ì–´ì˜ í˜„ìž¬ hp(static)
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
 
 
     // Start is called before the first frame update
@@ -44,17 +27,6 @@ public class GameDirector : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
-        Application.targetFrameRate = 60;       //¸ð¹ÙÀÏ È¯°æÀÏ °æ¿ì ÇÁ·¹ÀÓÀ» 60À¸·Î °íÁ¤
-        QualitySettings.vSyncCount = 0;
-
-        hp = maxHp;                             // HP¸¦ ÃÖ´ë °ª(3)À¸·Î ¼³Á¤
-        Time.timeScale = 1;                     // ½Ã°£ ½ºÄÉÀÏÀ» 1·Î ¼³Á¤ (ÀÏ¹Ý ¼Óµµ)
-                                                // Time.timeScale ¸Þ¼­µå¸¦ »ç¿ëÇÏ¿© ½Ã°£½ºÄÉÀÏÀ» 1·Î ¼±¾ð(½Ã°£ Èå¸§À» Á¤»ó ¼Óµµ·Î)
-                                                // Time.timeScale = 0(½Ã°£ Èå¸§À» ¸ØÃã)
-        UpdateRecipeCnt();                      // UI¿¡¼­ Àç·á °³¼ö ¾÷µ¥ÀÌÆ®
-        UpdateRecipeUI();                       // UI¿¡¼­ ·¹½ÃÇÇ¿Í Àç·á ÀÌ¹ÌÁö ¾÷µ¥ÀÌÆ®
-=======
         Application.targetFrameRate = 60;       //ëª¨ë°”ì¼ í™˜ê²½ì¼ ê²½ìš° í”„ë ˆìž„ì„ 60ìœ¼ë¡œ ê³ ì •
         QualitySettings.vSyncCount = 0;
 
@@ -64,24 +36,11 @@ public class GameDirector : MonoBehaviour
                                                 // Time.timeScale = 0(ì‹œê°„ íë¦„ì„ ë©ˆì¶¤)
         UpdateRecipeCnt();                      // UIì—ì„œ ìž¬ë£Œ ê°œìˆ˜ ì—…ë°ì´íŠ¸
         UpdateRecipeUI();                       // UIì—ì„œ ë ˆì‹œí”¼ì™€ ìž¬ë£Œ ì´ë¯¸ì§€ ì—…ë°ì´íŠ¸
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        UpdateHearthp();        // UI¿¡¼­ ÇÃ·¹ÀÌ¾îÀÇ HP¸¦ ³ªÅ¸³»´Â ÇÏÆ® ÀÌ¹ÌÁö ¾÷µ¥ÀÌÆ®
-        UpdateRecipeCnt();      // UI¿¡¼­ Àç·á °³¼ö ¾÷µ¥ÀÌÆ®
-        UpdateRecipeUI();       // UI¿¡¼­ ·¹½ÃÇÇ¿Í Àç·á ÀÌ¹ÌÁö ¾÷µ¥ÀÌÆ®
-
-        if (hp <= 0)            // hp°¡ 0ÀÌ¶ó¸é
-        {
-            Invoke("ActivateGameover", 3f);     // 3ÃÊ ÈÄ °ÔÀÓ ¿À¹ö ÆÐ³ÎÀ» È°¼ºÈ­
-            Invoke("GameOverChange", 5f);       // 5ÃÊ ÈÄ °ÔÀÓ ¿À¹ö ¾ÀÀ¸·Î ÀüÈ¯                                               
-                                                // Invoke("Æ¯Á¤ÇÔ¼ö", "xf") 
-        }                                       // Æ¯Á¤ ÇÔ¼ö¸¦ xÃÊ ÈÄ¿¡ ºÒ·¯¿À°Ô ÇÑ´Ù.
-=======
         UpdateHearthp();        // UIì—ì„œ í”Œë ˆì´ì–´ì˜ HPë¥¼ ë‚˜íƒ€ë‚´ëŠ” í•˜íŠ¸ ì´ë¯¸ì§€ ì—…ë°ì´íŠ¸
         UpdateRecipeCnt();      // UIì—ì„œ ìž¬ë£Œ ê°œìˆ˜ ì—…ë°ì´íŠ¸
         UpdateRecipeUI();       // UIì—ì„œ ë ˆì‹œí”¼ì™€ ìž¬ë£Œ ì´ë¯¸ì§€ ì—…ë°ì´íŠ¸
@@ -92,20 +51,10 @@ public class GameDirector : MonoBehaviour
             Invoke("GameOverChange", 5f);       // 5ì´ˆ í›„ ê²Œìž„ ì˜¤ë²„ ì”¬ìœ¼ë¡œ ì „í™˜                                               
                                                 // Invoke("íŠ¹ì •í•¨ìˆ˜", "xf") 
         }                                       // íŠ¹ì • í•¨ìˆ˜ë¥¼ xì´ˆ í›„ì— ë¶ˆëŸ¬ì˜¤ê²Œ í•œë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 
     public void UpdateRecipeCnt()
     {
-<<<<<<< HEAD
-        for (int i = 0; i < 4; i++)     // 0ºÎÅÍ 3±îÁöÀÇ ÀÎµ¦½º¸¦ »ç¿ëÇÏ¿© ¹Ýº¹
-        {   
-            try{
-                // ÀÜ¿© ·¹½ÃÇÇ °³¼ö¸¦ °¡Á®¿Í¼­ UI¿¡ ¾÷µ¥ÀÌÆ®
-                // Recipe.ShowLeftoverRecipe().Values.ToList()[i]¸¦ »ç¿ëÇÏ¿© ÀÜ¿© ·¹½ÃÇÇ °³¼ö¸¦ °¡Á®¿Â´Ù.
-                // gIngredient_cnt[i].GetComponent<Text>().text¸¦ »ç¿ëÇÏ¿©
-                // gIngredient_cnt ¹è¿­¿¡¼­ ÇöÀç ÀÎµ¦½º¿¡ ÇØ´çÇÏ´Â GameObjectÀÇ Text ÄÄÆ÷³ÍÆ®¸¦ °¡Á®¿Â´Ù.
-=======
         for (int i = 0; i < 4; i++)     // 0ë¶€í„° 3ê¹Œì§€ì˜ ì¸ë±ìŠ¤ë¥¼ ì‚¬ìš©í•˜ì—¬ ë°˜ë³µ
         {   
             try{
@@ -113,37 +62,17 @@ public class GameDirector : MonoBehaviour
                 // Recipe.ShowLeftoverRecipe().Values.ToList()[i]ë¥¼ ì‚¬ìš©í•˜ì—¬ ìž”ì—¬ ë ˆì‹œí”¼ ê°œìˆ˜ë¥¼ ê°€ì ¸ì˜¨ë‹¤.
                 // gIngredient_cnt[i].GetComponent<Text>().textë¥¼ ì‚¬ìš©í•˜ì—¬
                 // gIngredient_cnt ë°°ì—´ì—ì„œ í˜„ìž¬ ì¸ë±ìŠ¤ì— í•´ë‹¹í•˜ëŠ” GameObjectì˜ Text ì»´í¬ë„ŒíŠ¸ë¥¼ ê°€ì ¸ì˜¨ë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
                 gIngredient_cnt[i].GetComponent<Text>().text = "x" + Recipe.ShowLeftoverRecipe().Values.ToList()[i];
             }
             catch
             {
-<<<<<<< HEAD
-                // ¸¸¾à ¿¹¿Ü°¡ ¹ß»ýÇÑ´Ù¸é(°³¼ö°¡ ¾øÀ» °æ¿ì), ÇØ´ç ÀÎµ¦½º¿¡ ÇØ´çÇÏ´Â Text ÄÄÆ÷³ÍÆ®ÀÇ text ¼Ó¼ºÀ» ºñ¿öµÐ´Ù.
-=======
                 // ë§Œì•½ ì˜ˆì™¸ê°€ ë°œìƒí•œë‹¤ë©´(ê°œìˆ˜ê°€ ì—†ì„ ê²½ìš°), í•´ë‹¹ ì¸ë±ìŠ¤ì— í•´ë‹¹í•˜ëŠ” Text ì»´í¬ë„ŒíŠ¸ì˜ text ì†ì„±ì„ ë¹„ì›Œë‘”ë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
                 gIngredient_cnt[i].GetComponent<Text>().text = "";
             }
        }
     }
     public void UpdateRecipeUI()
     {
-<<<<<<< HEAD
-        for (int i = 0; i < 4; i++)     // 0ºÎÅÍ 3±îÁöÀÇ ÀÎµ¦½º¸¦ »ç¿ëÇÏ¿© ¹Ýº¹
-        {
-            try
-            {
-                // ingredientImages[i].enabled = true;¸¦ »ç¿ëÇÏ¿© ingredientImages ¹è¿­¿¡¼­
-                // ÇöÀç ÀÎµ¦½º¿¡ ÇØ´çÇÏ´Â Image ÄÄÆ÷³ÍÆ®ÀÇ È°¼ºÈ­ »óÅÂ¸¦ true·Î ¼³Á¤
-                // enabled´Â UnityÀÇ ÄÄÆ÷³ÍÆ®³ª °ÔÀÓ ¿ÀºêÁ§Æ®ÀÇ È°¼ºÈ­ ¿©ºÎ¸¦ ³ªÅ¸³»´Â ¼Ó¼º
-                ingredientImages[i].enabled = true;
-
-                // recipeSprites ¹è¿­¿¡¼­ ·¹½ÃÇÇ½ºÅ©¸³Æ®ÀÇ ·¹½ÃÇÇ ÀÎµ¦½º °ª¿¡ ÇØ´çÇÏ´Â ½ºÇÁ¶óÀÌÆ®·Î ¼³Á¤
-                recipeImage.sprite = recipeSprites[Recipe.RecipeIndex];
-
-                // Àç·á ÀÌ¹ÌÁö ¹è¿­¿¡¼­ ÀÜ¿© ·¹½ÃÇÇÀÇ Å°(Àç·á)¸¦ °¡Á®¿Â ÈÄ Å°¿¡ ÇØ´çÇÏ´Â Àç·á ÀÌ¹ÌÁö ½ºÇÁ¶óÀÌÆ® Àû¿ë
-=======
         for (int i = 0; i < 4; i++)     // 0ë¶€í„° 3ê¹Œì§€ì˜ ì¸ë±ìŠ¤ë¥¼ ì‚¬ìš©í•˜ì—¬ ë°˜ë³µ
         {
             try
@@ -157,16 +86,11 @@ public class GameDirector : MonoBehaviour
                 recipeImage.sprite = recipeSprites[Recipe.RecipeIndex];
 
                 // ìž¬ë£Œ ì´ë¯¸ì§€ ë°°ì—´ì—ì„œ ìž”ì—¬ ë ˆì‹œí”¼ì˜ í‚¤(ìž¬ë£Œ)ë¥¼ ê°€ì ¸ì˜¨ í›„ í‚¤ì— í•´ë‹¹í•˜ëŠ” ìž¬ë£Œ ì´ë¯¸ì§€ ìŠ¤í”„ë¼ì´íŠ¸ ì ìš©
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
                 ingredientImages[i].sprite = ingredientSprites[Recipe.ShowLeftoverRecipe().Keys.ToList()[i]];
             }
             catch
             {
-<<<<<<< HEAD
-                //¸¸¾à ¿¹¿Ü°¡ ¹ß»ýÇÑ´Ù¸é ingredientImages ¹è¿­¿¡¼­ ÇöÀç ÀÎµ¦½º¿¡ ÇØ´çÇÏ´Â Image ÄÄÆ÷³ÍÆ®ÀÇ È°¼ºÈ­ »óÅÂ¸¦ false·Î ¼³Á¤
-=======
                 //ë§Œì•½ ì˜ˆì™¸ê°€ ë°œìƒí•œë‹¤ë©´ ingredientImages ë°°ì—´ì—ì„œ í˜„ìž¬ ì¸ë±ìŠ¤ì— í•´ë‹¹í•˜ëŠ” Image ì»´í¬ë„ŒíŠ¸ì˜ í™œì„±í™” ìƒíƒœë¥¼ falseë¡œ ì„¤ì •
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
                 ingredientImages[i].enabled = false;
             }
         }
@@ -174,17 +98,6 @@ public class GameDirector : MonoBehaviour
 
     private void UpdateHearthp()
     {
-<<<<<<< HEAD
-        for (int i = 0; i < maxHp; i++)             //0ºÎÅÍ maxHp(3)±îÁöÀÇ ÀÎµ¦½º¸¦ »ç¿ëÇÏ¿© ¹Ýº¹
-        {
-            if (i < hp)                             // hp°¡ iº¸´Ù ÀÛÀ» °æ¿ì
-            {
-                heartImages[i].enabled = true;      // enabled = true¸¦ »ç¿ëÇÏ¿© hpÀÌ¹ÌÁö ¹è¿­À» UI·Î ³ªÅ¸³½´Ù.
-            }
-            else
-            {
-                heartImages[i].enabled = false;     // ±×°Ô ¾Æ´Ï¶ó¸é hp ÀÌ¹ÌÁö ¹è¿­ UI¸¦ ºñÈ°¼ºÈ­ ½ÃÅ´
-=======
         for (int i = 0; i < maxHp; i++)             //0ë¶€í„° maxHp(3)ê¹Œì§€ì˜ ì¸ë±ìŠ¤ë¥¼ ì‚¬ìš©í•˜ì—¬ ë°˜ë³µ
         {
             if (i < hp)                             // hpê°€ ië³´ë‹¤ ìž‘ì„ ê²½ìš°
@@ -194,26 +107,17 @@ public class GameDirector : MonoBehaviour
             else
             {
                 heartImages[i].enabled = false;     // ê·¸ê²Œ ì•„ë‹ˆë¼ë©´ hp ì´ë¯¸ì§€ ë°°ì—´ UIë¥¼ ë¹„í™œì„±í™” ì‹œí‚´
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
             }
         }
     }
 
     public void ActivateGameover()
     {
-<<<<<<< HEAD
-        Gameover_Panel.SetActive(true);             // SetActive¸Þ¼­µå¸¦ È°¿ëÇÏ¿© ÇØ´ç GameoverÆÇ³Ú ¿ÀºêÁ§Æ®¸¦ true(È°¼ºÈ­) ½ÃÅ²´Ù.
-=======
         Gameover_Panel.SetActive(true);             // SetActiveë©”ì„œë“œë¥¼ í™œìš©í•˜ì—¬ í•´ë‹¹ GameoveríŒë„¬ ì˜¤ë¸Œì íŠ¸ë¥¼ true(í™œì„±í™”) ì‹œí‚¨ë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 
     public void GameOverChange()
     {
-<<<<<<< HEAD
-        SceneDirector.ChangeScene2();               // °ÔÀÓ¿À¹ö ½Ã ¾À µð·ºÅÍÀÇ ChangeScene2ÇÔ¼ö¸¦ ½ÇÇàÇÑ´Ù(finishScene ÀüÈ¯)
-=======
         SceneDirector.ChangeScene2();               // ê²Œìž„ì˜¤ë²„ ì‹œ ì”¬ ë””ë ‰í„°ì˜ ChangeScene2í•¨ìˆ˜ë¥¼ ì‹¤í–‰í•œë‹¤(finishScene ì „í™˜)
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 } 
--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,38 +1,9 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-<<<<<<< HEAD
-    public GameObject Panel_menu = null;                // ¸Ş´º ÆĞ³ÎÀ» ³ªÅ¸³»´Â °ÔÀÓ ¿ÀºêÁ§Æ®
-    public GameObject Toggle1 = null;                   // SFX Åä±ÛÀ» ³ªÅ¸³»´Â °ÔÀÓ ¿ÀºêÁ§Æ®
-    public GameObject Toggle2 = null;                   // BGM Åä±ÛÀ» ³ªÅ¸³»´Â °ÔÀÓ ¿ÀºêÁ§Æ®
-    public Image[] OnImage;                             // È°¼ºÈ­ ÀÌ¹ÌÁö ¹è¿­
-    public Image[] OffImage;                            // ºñÈ°¼ºÈ­ ÀÌ¹ÌÁö ¹è¿­
-
-    private bool[] isMuted = new bool[2]; // SFX¿Í BGMÀÇ À½¼Ò°Å ¿©ºÎ¸¦ ÀúÀåÇÏ´Â ¹è¿­
-
-    public void Click_Menu()    // ¸Ş´º ¹öÆ°À» Å¬¸¯ÇßÀ» °æ¿ì
-    {
-        Time.timeScale = 0;                             // °ÔÀÓ ½Ã°£À» ÀÏ½ÃÀûÀ¸·Î ¸ØÃä´Ï´Ù.
-        Panel_menu.SetActive(true);                     // ¸Ş´º ÆĞ³ÎÀ» È°¼ºÈ­ÇÕ´Ï´Ù.
-    }
-
-    public void Click_Continue() // ¸Ş´ºÆĞ³ÎÀÇ CONTINUE ¹öÆ° Å¬¸¯ÇßÀ» °æ¿ì
-    {
-        Time.timeScale = 1;                             // °ÔÀÓ ½Ã°£À» ´Ù½Ã ½ÃÀÛÇÕ´Ï´Ù.
-        Panel_menu.SetActive(false);                    // ¸Ş´º ÆĞ³ÎÀ» ºñÈ°¼ºÈ­ÇÕ´Ï´Ù.
-    }
-
-    public void Click_Exit() // ¸Ş´ºÆĞ³ÎÀÇ QUIT ¹öÆ° Å¬¸¯ÇßÀ» °æ¿ì
-    {
-        ChangeScene3();
-    }
-
-    public void ChangeScene3()
-=======
     public GameObject Panel_menu = null;                // ë©”ë‰´ íŒ¨ë„ì„ ë‚˜íƒ€ë‚´ëŠ” ê²Œì„ ì˜¤ë¸Œì íŠ¸
     public GameObject Toggle1 = null;                   // SFX í† ê¸€ì„ ë‚˜íƒ€ë‚´ëŠ” ê²Œì„ ì˜¤ë¸Œì íŠ¸
     public GameObject Toggle2 = null;                   // BGM í† ê¸€ì„ ë‚˜íƒ€ë‚´ëŠ” ê²Œì„ ì˜¤ë¸Œì íŠ¸
@@ -60,25 +31,12 @@ public class MenuController : MonoBehaviour
     }
 
     private void Scenechange() // ì”¬ ì „í™˜ í•¨ìˆ˜
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     {
         SceneManager.LoadScene("TitleScene");           // "TitleScene"ì„ ë¡œë“œí•˜ì—¬ ì”¬ì„ ì „í™˜í•©ë‹ˆë‹¤.
     }
 
     void Start()
     {
-<<<<<<< HEAD
-        Panel_menu.SetActive(false);
-        isMuted[0] = false; // SFX ÃÊ±â À½¼Ò°Å »óÅÂ ¼³Á¤
-        isMuted[1] = false; // BGM ÃÊ±â À½¼Ò°Å »óÅÂ ¼³Á¤
-        UpdateMuteImages();
-    }
-
-    public void ToggleMute(int index) // SFX¿Í BGM Åä±ÛÀ» ±¸ºĞÇÏ´Â ÀÎµ¦½º¸¦ ¹Ş½À´Ï´Ù.
-    {
-        isMuted[index] = !isMuted[index]; // ÇØ´ç Åä±ÛÀÇ À½¼Ò°Å »óÅÂ¸¦ ¹İÀü½ÃÅµ´Ï´Ù.
-        UpdateMuteImages();
-=======
         Panel_menu.SetActive(false);                    // ë©”ë‰´ íŒ¨ë„ì„ ì²˜ìŒì—ëŠ” ë¹„í™œì„±í™”í•©ë‹ˆë‹¤.
         UpdateMuteImages();                             // ìŒì†Œê±° ì´ë¯¸ì§€ë¥¼ ì—…ë°ì´íŠ¸í•©ë‹ˆë‹¤.
     }
@@ -93,23 +51,13 @@ public class MenuController : MonoBehaviour
     {
         isMuted2 = !isMuted2;                           // BGM í† ê¸€ì˜ ìŒì†Œê±° ì´ë¯¸ì§€ë¥¼ ë°˜ì „ì‹œí‚µë‹ˆë‹¤.
         UpdateMuteImages();                             // ìŒì†Œê±° ì´ë¯¸ì§€ë¥¼ ì—…ë°ì´íŠ¸í•©ë‹ˆë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 
     private void UpdateMuteImages() // ìŒì†Œê±° ì´ë¯¸ì§€ ë³€ê²½ í•¨ìˆ˜
     {
-<<<<<<< HEAD
-        // SFX¿Í BGM Åä±ÛÀÇ È°¼ºÈ­/ºñÈ°¼ºÈ­ ÀÌ¹ÌÁö¸¦ ¼³Á¤ÇÕ´Ï´Ù.
-        for (int i = 0; i < 2; i++)
-        {
-            OnImage[i].gameObject.SetActive(!isMuted[i]);
-            OffImage[i].gameObject.SetActive(isMuted[i]);
-        }
-=======
         OnImage[0].gameObject.SetActive(!isMuted1);     // SFX í† ê¸€ì˜ í™œì„±í™” ì´ë¯¸ì§€ë¥¼ ì„¤ì •í•©ë‹ˆë‹¤.
         OffImage[0].gameObject.SetActive(isMuted1);     // SFX í† ê¸€ì˜ ë¹„í™œì„±í™” ì´ë¯¸ì§€ë¥¼ ì„¤ì •í•©ë‹ˆë‹¤.
         OnImage[1].gameObject.SetActive(!isMuted2);     // BGM í† ê¸€ì˜ í™œì„±í™” ì´ë¯¸ì§€ë¥¼ ì„¤ì •í•©ë‹ˆë‹¤.
         OffImage[1].gameObject.SetActive(isMuted2);     // BGM í† ê¸€ì˜ ë¹„í™œì„±í™” ì´ë¯¸ì§€ë¥¼ ì„¤ì •í•©ë‹ˆë‹¤.
->>>>>>> 2614fe62da4a1ecd041231f023e85214d0fc979d
     }
 }
